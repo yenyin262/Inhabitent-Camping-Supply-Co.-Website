a649d70d3d6b06bc407f33c799fa72b5a7983ca8
Yen Yin Hing <yenyinf287@gmail.com>
2019-02-12 15:15:37 -0800

    fix front-page style in journal button and added * in find us form

--- themes/inhabitent_P4/template-parts/content-single-blog-view.php
+++ themes/inhabitent_P4/template-parts/content-single-blog-view.php
@@ -8,9 +8,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-   
-
 	<header class="blog-entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
